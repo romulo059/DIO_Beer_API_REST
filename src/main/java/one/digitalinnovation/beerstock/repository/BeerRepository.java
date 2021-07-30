8f188cc721b9ec3bd146b62c3b8b82e5c75607ab
Rômulo Guedes <romuloguedes059@gmail.com>
2021-07-30 11:59:59 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/beerstock/repository/BeerRepository.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.beerstock.repository;
+
+import one.digitalinnovation.beerstock.entity.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name); // optional java 8 que evita erros (nullpointer)
+}
